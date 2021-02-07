bab32f949253cdaa4c48415b9dc45ea5a22543e9
Caio <caiowgcw@gmail.com>
2021-02-06 22:39:20 -0300

    GET POST DELETE WORKING

--- scripts/gradesController.js
+++ scripts/gradesController.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { promises as fs } from "fs";
-
+import { createSecureContext } from "tls";
 
 const { readFile, writeFile } = fs;
 const router = express.Router();
@@ -21,9 +21,7 @@ router.post("/", async (req, res, next) => {
       subject: grades.subject,
       type: grades.type,
       value: grades.value,
-      timestamp: new Date().toISOString()
-
-
+      timestamp: new Date().toISOString(),
     };
     data.grades.push(grades);
 
@@ -36,14 +34,57 @@ router.post("/", async (req, res, next) => {
 });
 
 router.get("/", async (req, res, next) => {
-    try {
-      const data = JSON.parse(await readFile("grades.json"));
-      delete data.nextId;
-      res.send(data);
-    } catch (err) {
-      next(err);
-    }
-  });
+  try {
+    const data = JSON.parse(await readFile("grades.json"));
+    delete data.nextId;
+    res.send(data);
+  } catch (err) {
+    next(err);
+  }
+});
 
+router.get("/:id", async (req, res, next) => {
+  try {
+    const data = JSON.parse(await readFile("grades.json", "utf8"));
+    const grade = data.grades.find(
+      (grade) => grade.id === parseInt(req.params.id)
+    );
+    res.send(grade);
+  } catch (err) {
+    next(err);
+  }
+});
+router.post("/:total", async (req, res, next) => {
+  try {
+    const data = JSON.parse(await readFile("grades.json", "utf8"));
+    const grade = data.grades.filter(
+      (grade) =>
+        grade.student === req.body.student && grade.subject === req.body.subject
+    );
 
-  export default router;
+    const total = grade.reduce((acc, cur) => {
+      return acc + cur.value;
+    }, 0);
+
+    // const somaMedias = grade.reduce((acc, cur) => acc + cur.value);
+
+    res.send({total});
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const data = JSON.parse(await readFile("grades.json", "utf8"));
+    data.grades = data.grades.filter(
+      (grade) => grade.id !== parseInt(req.params.id)
+    );
+    await writeFile("grades.json", JSON.stringify(data, null, 2));
+    res.end();
+  } catch (err) {
+    next(err);
+  }
+});
+
+export default router;
